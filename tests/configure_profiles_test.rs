c36d243280249e1a08504983815ddf54382f8a44
Matt Straathof <matt.straat@gmail.com>
2021-11-29 14:03:32 -0800

    fix: integration tests on default profile

--- tests/configure_profiles_test.rs
+++ tests/configure_profiles_test.rs
@@ -6,17 +6,9 @@ mod tests {
     use home::home_dir;
 
     #[tokio::test]
-    async fn configure_momento_default_profile() {
-        let home = home_dir().unwrap();
-        let momento_dir = format!("{}/.momento", home.clone().display());
-        if Path::new(&momento_dir).exists() {
-            panic!("These integration tests test reading profiles from disk, and create a ~/.momento directory to test this.
-            To avoid overriding existing profiles, this error has been thrown.
-            If you a want to run these tests, run 'mv ~/.momento ~/.momento.bac' to save the current profiles.
-            After these tests complete run 'mv ~/.momento.bac ~/.momento' to restore the profiles");
-        };
+    async fn configure_momento_named_profile() {
         let mut cmd = Command::cargo_bin("momento").unwrap();
-        cmd.args(&["configure"])
+        cmd.args(&["configure", "--profile", "TEST_PROFILE"])
             .write_stdin("token\ncache\n999")
             .assert()
             .success();
