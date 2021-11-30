26af178cce5739d2fcb5f9b7d245c97642f543bd
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2021-11-30 10:34:24 -0800

    fix: momento configure when user is first configuring momento (#25)

--- src/utils/file.rs
+++ src/utils/file.rs
@@ -5,7 +5,6 @@ use home::home_dir;
 use log::debug;
 use serde::de;
 use tokio::fs::{self, File};
-use toml::de::Error;
 
 pub fn get_credentials_file_path() -> String {
     let momento_home = get_momento_dir();
@@ -22,17 +21,14 @@ pub fn get_momento_dir() -> String {
     return format!("{}/.momento", home.clone().display());
 }
 
-pub async fn read_toml_file<T: de::DeserializeOwned>(path: &str) -> Result<T, Error> {
+pub async fn read_toml_file<T: de::DeserializeOwned>(path: &str) -> Result<T, String> {
     let toml_str = match fs::read_to_string(&path).await {
         Ok(s) => s,
-        Err(_) => panic!(
-            "faile to read file {}, please run 'momento configure'",
-            path
-        ),
+        Err(e) => return Err(format!("failed to read file: {}", e)),
     };
     match toml::from_str::<T>(&toml_str) {
         Ok(c) => Ok(c),
-        Err(e) => Err(e),
+        Err(e) => Err(format!("failed to parse file: {}", e)),
     }
 }
 
@@ -73,14 +69,4 @@ pub async fn set_file_read_write(path: &str) -> std::io::Result<()> {
     perms.set_mode(0o600);
     fs::set_permissions(path, perms).await?;
     Ok(())
-}
-
-pub async fn create_dir_if_not_exists(path: &str) {
-    if !Path::new(path).exists() {
-        let res = fs::create_dir_all(path).await;
-        match res {
-            Ok(_) => debug!("created directory {}", path),
-            Err(e) => panic!("failed to created directory {}, error: {}", path, e),
-        }
-    }
 }
