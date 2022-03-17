8548963908ccfdab9ae14503c79ae8f9c14e9820
erikatharp <eem@momentohq.com>
2022-03-17 10:58:52 -0700

    add momento configure basic test; add important notes to CONTRIBUTING.md

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -44,6 +44,19 @@ export TEST_PROFILE=<YOUR_TEST_PROFILE>
 cargo clippy --all-targets --all-features -- -D warnings
 ```
 
+<br>
+
+:warning: Important notes on running `cargo test --test configure_profiles_test`
+
+```
+export TEST_AUTH_TOKEN=<YOUR_TEST_AUTH_TOKEN>
+export TEST_CACHE_DEFAULT=<YOUR_TEST_CACHE_DEFAULT>
+cargo test --test configure_profile_test
+```
+
+- If you already have existing credentials and config files locally, running `cargo test --test configure_profiles_test` with provided `TEST_AUTH_TOKEN` will overwrite values for token your `default` profile.
+- The value for `TEST_CACHE_DEFAULT` needs to match the cache value your `default` profile and the cache needs to exist. However, this cache will be deleted after this test runs successfully.
+
 ### Deploying
 
 After merge a pr will be created in this repo https://github.com/momentohq/homebrew-tap. Once the pr passes all checks, approve the pr and label is as `pr-pull`. It will then get automatically merged by the homebrew bot, and a release will be created for it.
--- tests/configure_profiles_test.rs
+++ tests/configure_profiles_test.rs
@@ -3,12 +3,42 @@ mod tests {
 
     use assert_cmd::Command;
 
-    #[tokio::test]
     async fn configure_momento_default_profile() {
+        let test_auth_token = std::env::var("TEST_AUTH_TOKEN").unwrap();
         let mut cmd = Command::cargo_bin("momento").unwrap();
         cmd.args(&["configure"])
-            .write_stdin("token\ncache\n999")
+            .write_stdin(format!("{}", test_auth_token))
             .assert()
             .success();
+    }
+
+    async fn momento_cache_set_default_profile() {
+        let mut cmd = Command::cargo_bin("momento").unwrap();
+        cmd.args(&["cache", "set", "--key", "key", "--value", "value"])
+            .assert()
+            .success();
+    }
+
+    async fn momento_cache_get_default_profile() {
+        let mut cmd = Command::cargo_bin("momento").unwrap();
+        cmd.args(&["cache", "get", "--key", "key"])
+            .assert()
+            .stdout("value\n");
+    }
+
+    async fn momento_cache_delete_default_profile() {
+        let test_cache_default = std::env::var("TEST_CACHE_DEFAULT").unwrap();
+        let mut cmd = Command::cargo_bin("momento").unwrap();
+        cmd.args(&["cache", "delete", "--name", &test_cache_default])
+            .assert()
+            .success();
+    }
+
+    #[tokio::test]
+    async fn momento_configured_default_profile() {
+        configure_momento_default_profile().await;
+        momento_cache_set_default_profile().await;
+        momento_cache_get_default_profile().await;
+        momento_cache_delete_default_profile().await;
     }
 }
