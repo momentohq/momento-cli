e872932939f47b8f3eb58c3fc0982e446a421948
Michael Landis <michael@momentohq.com>
2022-10-12 10:53:08 -0700

    fix: revert typo

--- tests/momento_additional_profile.rs
+++ tests/momento_additional_profile.rs
@@ -41,7 +41,8 @@ mod tests {
         let test_profile = std::env::var("TEST_PROFILE").unwrap();
         let mut cmd = Command::cargo_bin("momento").unwrap();
         cmd.args(&["cache", "get", "--key", "key", "--profile", &test_profile])
-            .assert();
+            .assert()
+            .stdout("value\n");
     }
 
     async fn momento_cache_list_with_profile() {
