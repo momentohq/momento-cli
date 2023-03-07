2f3d42bd13caa6cdc3156666a281855dbe0cc757
Matt Straathof <matt.straat@gmail.com>
2023-03-07 14:56:12 -0800

    chore: use humantime crate

--- momento/src/commands/tokens/mod.rs
+++ momento/src/commands/tokens/mod.rs
@@ -1,4 +1,3 @@
-use duration_str::parse;
 use momento::requests::generate_api_token_request::TokenExpiry;
 
 use crate::{
@@ -15,20 +14,20 @@ pub async fn generate_api_token(
     let expiry = if never_expire {
         TokenExpiry::Never {}
     } else {
-        let seconds = parse(
-            valid_for
-                .expect("oneof --valid-for, or --never-expire, must be set")
-                .as_str(),
-        )
-        .expect("unable to parse valid_for duration")
-        .as_secs();
+        let seconds = valid_for
+            .expect("oneof --valid-for, or --never-expire, must be set")
+            .as_str()
+            .parse::<humantime::Duration>()
+            .expect("unable to parse valid_for duration")
+            .as_secs();
         TokenExpiry::Expires {
             valid_for_seconds: seconds as u32,
         }
     };
     let mut client = get_momento_client(auth_token, endpoint).await?;
     print_whatever_this_is_as_json(
-        &interact_with_momento("generating...", client.generate_api_token(expiry)).await?,
+        &interact_with_momento("generating api token...", client.generate_api_token(expiry))
+            .await?,
     );
     Ok(())
 }
