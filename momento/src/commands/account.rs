38b11492fbbab883e956f694a6a8a496f821922a
Ruth Linehan <1530016+rlinehan@users.noreply.github.com>
2023-06-09 15:38:49 -0700

    fix: linter

--- momento/src/commands/account.rs
+++ momento/src/commands/account.rs
@@ -1,10 +1,10 @@
 use crate::error::CliError;
 
 pub async fn signup_decommissioned() -> Result<(), CliError> {
-    return Err(CliError {
+    Err(CliError {
         msg: r"This command has been decommissioned!
 Please go to the console to sign up for Momento and generate a token:
 https://console.gomomento.com"
             .to_string(),
-    });
+    })
 }
