976f1095e276c8edd17a7b2a6e553c44fdf0428d
Tyler Burdsall <10414594+tylerburdsall@users.noreply.github.com>
2025-10-07 13:38:47 -0700

    chore: for topics subscribe messages, use Display instead of Debug (#343)

--- momento/src/commands/topic/mod.rs
+++ momento/src/commands/topic/mod.rs
@@ -6,7 +6,7 @@ use crate::utils::console::console_data;
 pub async fn print_subscription(mut subscription: Subscription) -> MomentoResult<()> {
     while let Some(item) = subscription.next().await {
         match item.kind {
-            momento::topics::ValueKind::Text(text) => console_data!("{:?}", text),
+            momento::topics::ValueKind::Text(text) => console_data!("{text}"),
             momento::topics::ValueKind::Binary(binary) => {
                 console_data!("{:?}", binary)
             }
