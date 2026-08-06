c558861c9c5bb6dd10e2044a47abdcbdf814a584
Kira Hammond <kirmar@stormyriver.com>
2026-08-05 22:21:55 -0700

    chore: share HTTP API logic for capacity pools

--- momento/src/commands/capacity_pool/pool_cli.rs
+++ momento/src/commands/capacity_pool/pool_cli.rs
@@ -1,9 +1,9 @@
-use super::utils::{ExplicitProvisioning, ExplicitProvisioningUpdate, PoolError, PoolResponse};
+use super::utils::{
+    call_pool_http_api, ExplicitProvisioning, ExplicitProvisioningUpdate, Response,
+};
 use crate::{error::CliError, utils::console::console_data};
 
 use http::Method;
-use log::{info, warn};
-use reqwest;
 use serde_json;
 
 pub async fn create_pool(
@@ -15,7 +15,6 @@ pub async fn create_pool(
     replicas_per_shard: u32,
     zones: Vec<String>,
 ) -> Result<(), CliError> {
-    let request_url = format!("{endpoint}/capacity_pool/{name}");
     let data = serde_json::json!({
         "provisioning": {
             "explicit": ExplicitProvisioning {
@@ -25,51 +24,21 @@ pub async fn create_pool(
                 zones,
             }
         }
-    })
-    .to_string();
-
-    let req_client = reqwest::Client::builder().build()?;
-    let response = req_client
-        .request(Method::POST, &request_url)
-        .body(data)
-        .header("authorization", &auth_token)
-        .header("content-type", "application/json")
-        .send()
-        .await?;
-    let status = response.status();
-    if status.is_success() {
-        let response_text = response.text().await?;
-        match serde_json::from_str::<PoolResponse>(response_text.as_str()) {
-            Ok(pool) => {
-                info!("Response sent back by {name} pool creation:\n{pool:#?}");
-                console_data!(
-                    "Creating pool! Name: {}, Status: {}, Provisioning: {:#?}",
-                    pool.name,
-                    pool.status,
-                    pool.provisioning.explicit,
-                );
-            }
-            Err(err) => {
-                warn!("Can't parse response from {name} pool creation:\n{response_text}\n{err}");
-                console_data!("Creating pool! {response_text}");
-            }
-        };
-        Ok(())
-    } else {
-        let error_text = response.text().await?;
-        Err(CliError::new(if error_text.is_empty() {
-            format!("{status}")
-        } else {
-            let error_message = match serde_json::from_str::<PoolError>(error_text.as_str()) {
-                Ok(error) => error
-                    .detail
-                    .unwrap_or(error.message.unwrap_or(error_text.clone())),
-                Err(_) => error_text.clone(),
-            };
-            format!("{status}: {error_message}")
-        })
-        .with_details(error_text))
-    }
+    });
+    match call_pool_http_api(Method::POST, endpoint, auth_token, name, Some(data)).await? {
+        Response::Parsed(pool) => {
+            console_data!(
+                "Creating pool! Name: {}, Status: {}, Provisioning: {:#?}",
+                pool.name,
+                pool.status,
+                pool.provisioning.explicit,
+            );
+        }
+        Response::Unparseable(response_text) => {
+            console_data!("Creating pool! {response_text}");
+        }
+    };
+    Ok(())
 }
 
 pub async fn get_status(
@@ -77,43 +46,15 @@ pub async fn get_status(
     auth_token: String,
     name: String,
 ) -> Result<(), CliError> {
-    let request_url = format!("{endpoint}/capacity_pool/{name}");
-
-    let req_client = reqwest::Client::builder().build()?;
-    let response = req_client
-        .request(Method::GET, &request_url)
-        .header("authorization", &auth_token)
-        .send()
-        .await?;
-    let status = response.status();
-    if status.is_success() {
-        let response_text = response.text().await?;
-        match serde_json::from_str::<PoolResponse>(response_text.as_str()) {
-            Ok(pool) => {
-                info!("Response sent back by {name} pool describe:\n{pool:#?}");
-                console_data!("Pool status for {}: {}", pool.name, pool.status);
-            }
-            Err(err) => {
-                warn!("Can't parse response from {name} pool describe:\n{response_text}\n{err}");
-                console_data!("{response_text}");
-            }
-        };
-        Ok(())
-    } else {
-        let error_text = response.text().await?;
-        Err(CliError::new(if error_text.is_empty() {
-            format!("{status}")
-        } else {
-            let error_message = match serde_json::from_str::<PoolError>(error_text.as_str()) {
-                Ok(error) => error
-                    .detail
-                    .unwrap_or(error.message.unwrap_or(error_text.clone())),
-                Err(_) => error_text.clone(),
-            };
-            format!("{status}: {error_message}")
-        })
-        .with_details(error_text))
-    }
+    match call_pool_http_api(Method::GET, endpoint, auth_token, name, None).await? {
+        Response::Parsed(pool) => {
+            console_data!("Pool status for {}: {}", pool.name, pool.status);
+        }
+        Response::Unparseable(response_text) => {
+            console_data!("{response_text}");
+        }
+    };
+    Ok(())
 }
 
 pub async fn update_pool(
@@ -125,7 +66,6 @@ pub async fn update_pool(
     replicas_per_shard: Option<u32>,
     zones: Option<Vec<String>>,
 ) -> Result<(), CliError> {
-    let request_url = format!("{endpoint}/capacity_pool/{name}");
     let data = serde_json::json!({
         "provisioning": {
             "explicit": ExplicitProvisioningUpdate {
@@ -135,49 +75,19 @@ pub async fn update_pool(
                 zones,
             }
         }
-    })
-    .to_string();
-
-    let req_client = reqwest::Client::builder().build()?;
-    let response = req_client
-        .request(Method::PATCH, &request_url)
-        .body(data)
-        .header("authorization", &auth_token)
-        .header("content-type", "application/json")
-        .send()
-        .await?;
-    let status = response.status();
-    if status.is_success() {
-        let response_text = response.text().await?;
-        match serde_json::from_str::<PoolResponse>(response_text.as_str()) {
-            Ok(pool) => {
-                info!("Response sent back by {name} pool update:\n{pool:#?}");
-                console_data!(
-                    "Updating pool! Name: {}, Status: {}, Provisioning: {:#?}",
-                    pool.name,
-                    pool.status,
-                    pool.provisioning.explicit,
-                );
-            }
-            Err(err) => {
-                warn!("Can't parse response from {name} pool update:\n{response_text}\n{err}");
-                console_data!("Updating pool! {response_text}");
-            }
-        };
-        Ok(())
-    } else {
-        let error_text = response.text().await?;
-        Err(CliError::new(if error_text.is_empty() {
-            format!("{status}")
-        } else {
-            let error_message = match serde_json::from_str::<PoolError>(error_text.as_str()) {
-                Ok(error) => error
-                    .detail
-                    .unwrap_or(error.message.unwrap_or(error_text.clone())),
-                Err(_) => error_text.clone(),
-            };
-            format!("{status}: {error_message}")
-        })
-        .with_details(error_text))
-    }
+    });
+    match call_pool_http_api(Method::PATCH, endpoint, auth_token, name, Some(data)).await? {
+        Response::Parsed(pool) => {
+            console_data!(
+                "Updating pool! Name: {}, Status: {}, Provisioning: {:#?}",
+                pool.name,
+                pool.status,
+                pool.provisioning.explicit,
+            );
+        }
+        Response::Unparseable(response_text) => {
+            console_data!("Updating pool! {response_text}");
+        }
+    };
+    Ok(())
 }
